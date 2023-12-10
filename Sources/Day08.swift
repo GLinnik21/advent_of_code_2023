408d2982040666d0369f4c596f690f43bb4ce879
Gleb Linnik <glinnik@yandex-team.ru>
2023-12-11 01:02:17 +0300

    Day 8 Part 2

--- Sources/Day08.swift
+++ Sources/Day08.swift
@@ -17,16 +17,33 @@ struct Day08: AdventDay {
     }
   }
 
-  func part1() -> Any {
-    var currentNode = "AAA"
+  fileprivate func steps(from node: String, to condition: (String) -> Bool) -> Int {
+    var currentNode = node
     var steps = 0
 
     for direction in instructions.cycled() {
       steps += 1
       currentNode = (direction == "L") ? mapping[currentNode]!.0 : mapping[currentNode]!.1
-      if currentNode == "ZZZ" { break }
+      if condition(currentNode) { break }
     }
 
     return steps
+  }
+
+  func part1() -> Any {
+    return steps(from: "AAA") { $0 == "ZZZ" }
+  }
+
+  private func gcd(_ a: Int, _ b: Int) -> Int {
+    return b == 0 ? a : gcd(b, a % b)
+  }
+
+  private func lcm(_ a: Int, _ b: Int) -> Int {
+    return (a * b) / gcd(a, b)
+  }
+
+  func part2() -> Any {
+    let nodes = mapping.keys.filter { $0.last == "A" }
+    return nodes.map { steps(from: $0, to: { $0.last == "Z" }) }.reduce(1, lcm)
   }
 }
--- Tests/Day08.swift
+++ Tests/Day08.swift
@@ -29,4 +29,21 @@ final class Day08Tests: XCTestCase {
         """)
     XCTAssertEqual(String(describing: challenge.part1()), "6")
   }
+
+  func testPart2() throws {
+    let challenge = Day08(
+      data: """
+        LR
+
+        11A = (11B, XXX)
+        11B = (XXX, 11Z)
+        11Z = (11B, XXX)
+        22A = (22B, XXX)
+        22B = (22C, 22C)
+        22C = (22Z, 22Z)
+        22Z = (22B, 22B)
+        XXX = (XXX, XXX)
+        """)
+    XCTAssertEqual(String(describing: challenge.part2()), "6")
+  }
 }
