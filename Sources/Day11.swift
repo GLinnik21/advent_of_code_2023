bb4ed3ba97fbbfa880cabc7f8f3b748556d9b233
Gleb Linnik <glinnik@yandex-team.ru>
2023-12-12 01:02:03 +0300

    Refactor Day 11

--- Sources/Day11.swift
+++ Sources/Day11.swift
@@ -1,7 +1,9 @@
 import Algorithms
 
 struct Day11: AdventDay {
-  private let galaxies: [(x: Int, y: Int)]
+  typealias Galaxy = (x: Int, y: Int)
+
+  private let galaxies: [Galaxy]
   private let verticalSpaces: Set<Int>
   private let horizontalSpaces: Set<Int>
   private let expansion: Int
@@ -34,30 +36,32 @@ struct Day11: AdventDay {
     self.expansion = expansion
   }
 
+  private func distance(of pairOfGalaxies: [(Galaxy)], expansionFactor: Int = 2) -> Int {
+    guard let firstGalaxy = pairOfGalaxies.first, let lastGalaxy = pairOfGalaxies.last else {
+      return 0
+    }
+
+    let (x1, y1) = (firstGalaxy.x, firstGalaxy.y)
+    let (x2, y2) = (lastGalaxy.x, lastGalaxy.y)
+    let xRange = min(x1, x2)...max(x1, x2)
+    let yRange = min(y1, y2)...max(y1, y2)
+
+    let verticalSpace = verticalSpaces.filter { xRange.contains($0) }.count * (expansionFactor - 1)
+    let horizontalSpace =
+      horizontalSpaces.filter { yRange.contains($0) }.count * (expansionFactor - 1)
+
+    return abs(x1 - x2) + abs(y1 - y2) + verticalSpace + horizontalSpace
+  }
+
   func part1() -> Any {
     galaxies.combinations(ofCount: 2).reduce(0) { sum, pairOfGalaxies in
-      let (x1, x2) = (pairOfGalaxies.first!.x, pairOfGalaxies.last!.x)
-      let (y1, y2) = (pairOfGalaxies.first!.y, pairOfGalaxies.last!.y)
-      let xRange = min(x1, x2)...max(x1, x2)
-      let yRange = min(y1, y2)...max(y1, y2)
-      let verticalSpace = verticalSpaces.filter { xRange.contains($0) }.count
-      let horizontalSpace = horizontalSpaces.filter { yRange.contains($0) }.count
-
-      return sum + abs(x1 - x2) + abs(y1 - y2) + verticalSpace + horizontalSpace
+      sum + distance(of: pairOfGalaxies)
     }
   }
 
   func part2() -> Any {
     galaxies.combinations(ofCount: 2).reduce(0) { sum, pairOfGalaxies in
-      let (x1, x2) = (pairOfGalaxies.first!.x, pairOfGalaxies.last!.x)
-      let (y1, y2) = (pairOfGalaxies.first!.y, pairOfGalaxies.last!.y)
-      let xRange = min(x1, x2)...max(x1, x2)
-      let yRange = min(y1, y2)...max(y1, y2)
-
-      let verticalSpace = verticalSpaces.filter { xRange.contains($0) }.count * (expansion - 1)
-      let horizontalSpace = horizontalSpaces.filter { yRange.contains($0) }.count * (expansion - 1)
-
-      return sum + abs(x1 - x2) + abs(y1 - y2) + verticalSpace + horizontalSpace
+      sum + distance(of: pairOfGalaxies, expansionFactor: expansion)
     }
   }
 }
